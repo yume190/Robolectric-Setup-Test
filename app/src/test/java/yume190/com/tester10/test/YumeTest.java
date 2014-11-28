a1391cc25c82e0f011e2112e263529d3cb32f694
yume190 <yume190@gmail.com>
2014-11-28 17:07:40 +0800

    first commit

---
+++ app/src/test/java/yume190/com/tester10/test/YumeTest.java
@@ -0,0 +1,24 @@
+package yume190.com.tester10.test;
+
+import android.test.AndroidTestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(emulateSdk = 18)
+public class YumeTest extends AndroidTestCase {
+
+    @Test
+    public void testYume1() throws Exception{
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void testYume2() throws Exception{
+        assertEquals(1,1);
+    }
+
+}
